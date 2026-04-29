ea881f6710ab4f0ddae32015c63a8a51bee4597e
Yogye Berry <yogyeberry@gmail.com>
2026-04-29 09:18:50 +0530

    Add Navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar(){
+    return(
+        <div>
+            <a href="/">Home</a>
+            <a href="/problems">Problems</a>
+            <a href="/contests">Contests</a>
+            <a href="/editor">Editor</a>
+            <a href="/profile">Profile</a>
+            <a href="/login">Login</a>
+            <a href="/signup">Signup</a>
+            <a href="/logout">Logout</a>
+            <a href="/register">Register</a>
+            <a href="/register/student">Student</a>
+            <a href="/register/teacher">Teacher</a>
+            <a href="/register/company">Company</a>
+        </div>
+    );
+}
+
+export default Navbar;
