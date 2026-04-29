da0e02d50f24bec14912b8d6ebb975199a3d2e36
Yogye Berry <yogyeberry@gmail.com>
2026-04-29 09:25:16 +0530

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,7 @@
 function Navbar(){
     return(
         <div>
-            <a href="/">Home</a>
-            <a href="/problems">Problems</a>
-            <a href="/contests">Contests</a>
-            <a href="/editor">Editor</a>
-            <a href="/profile">Profile</a>
-            <a href="/login">Login</a>
-            <a href="/signup">Signup</a>
-            <a href="/logout">Logout</a>
-            <a href="/register">Register</a>
-            <a href="/register/student">Student</a>
+            
         </div>
     );
 }
