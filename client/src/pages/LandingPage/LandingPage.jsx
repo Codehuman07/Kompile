151b29bfdcefa0b256cb024f21d00f44ef4a5b02
Yogye Berry <yogyeberry@gmail.com>
2026-04-30 19:32:04 +0530

    feat: add GridComponent for background styling

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "../../components/Navbar";
-
+import GridComponent from "../../components/GridComponent";
 export default function LandingPage() {
   return (
     <>
       <Navbar />
-      <div className="w-full h-screen bg-[#141413] flex items-center justify-center">
-        <h1 className="text-8xl text-white font-bold">Welcome to Kompile</h1>
-      </div>
+      <GridComponent>
+        <div className="w-full h-screen flex items-center justify-center">
+          <h1 className="text-8xl text-white font-bold">Welcome to Kom<span className="text-orange-500">pile</span></h1>
+        </div>
+      </GridComponent>
     </>
   );
 }
