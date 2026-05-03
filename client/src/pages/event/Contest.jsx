0047072d00a8ef01cf9e13e3ffb029132c024533
“sarthak-gupta229Git config --global user.email “guptasarthak229@gmail.com <guptasarthak229@gmail.com>
2026-05-03 15:19:13 +0530

    feat: add contest page and register new /events route

---
+++ client/src/pages/event/Contest.jsx
@@ -0,0 +1,12 @@
+import GridComponent from "../../components/GridComponent";
+import Navbar from "../../components/Navbar";
+function Contest() {
+  return (
+    <>
+      <Navbar />
+      <GridComponent>Contest</GridComponent>
+    </>
+  );
+}
+
+export default Contest;
